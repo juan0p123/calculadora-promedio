60254f678f4b46a2c95ed2049fb5a2782d59fb95
Usuario <juanoportus@alu.unach.cl>
2026-04-13 14:57:21 -0400

    Logica correcta, escenario correcto

--- public/calculadora.js
+++ public/calculadora.js
@@ -1,8 +1,5 @@
 function calcularPromedio(n1, n2, n3, n4) {
-  // Error: lógica incorrecta agregando +10 de manera arbitraria
-  const promedio = n1 * 0.1 + n2 * 0.2 + n3 * 0.3 + n4 * 0.4 + 10;
-
-  return promedio; // Solo se devuelve el promedio
+  return n1 * 0.1 + n2 * 0.2 + n3 * 0.3 + n4 * 0.4;
 }
 
 module.exports = { calcularPromedio };
